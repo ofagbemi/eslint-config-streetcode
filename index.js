d416999356472d70c274064b86c1c9ef86f9e938
Jare Fagbemi <ofagbemi@stanford.edu>
2017-06-18 00:45:21 -0700

    Add env, globals, and rules

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ module.exports = {
   extends: 'airbnb',
   node: true,
   parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    React: true,
+  },
   rules: {
     'new-cap': 'off',
     'no-use-before-define': ['error', { functions: false }],
-    'prefer-rest-params': 'off'
+    'prefer-rest-params': 'off',
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+    'arrow-parens': ['error', 'as-needed'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/prop-types': 'off',
   },
 };
